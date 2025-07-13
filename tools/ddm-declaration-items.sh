7a617785353c261a643286798ba64e1381b1e3d1
Nate Walck <github@natewalck.com>
2025-07-13 11:46:03 -0400

    Add auth for nanohub support

--- tools/ddm-declaration-items.sh
+++ tools/ddm-declaration-items.sh
@@ -2,7 +2,12 @@
 
 URL="${BASE_URL}/declaration-items"
 
+if [ "x$API_USER" = "x" ]; then
+    API_USER="kmfddm"
+fi
+
 curl \
     $CURL_OPTS \
     -H "X-Enrollment-ID: $1" \
+    -u "$API_USER:$API_KEY" \
     "$URL"
--- tools/ddm-declaration.sh
+++ tools/ddm-declaration.sh
@@ -2,7 +2,12 @@
 
 URL="${BASE_URL}/declaration/$2"
 
+if [ "x$API_USER" = "x" ]; then
+    API_USER="kmfddm"
+fi
+
 curl \
     $CURL_OPTS \
     -H "X-Enrollment-ID: $1" \
+    -u "$API_USER:$API_KEY" \
     "$URL"
